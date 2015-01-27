3a3a4d4f974a4e76161e9c2fcf128f8b06ca185b
Mike Mouse III <mouse166@gmail.com>
2015-01-27 15:25:08 +0300

    prepare data oject for change orm

--- database/database.js
+++ database/database.js
@@ -4,9 +4,9 @@ var dbproxy = require('./dbproxy.js');
 
 exports.connect = function(callback) {
 	console.log("Connecting to database...");
-	dbproxy.connect(config.database);
+	dbproxy.db.connect(config.database);
 
-	db = dbproxy.connection;
+	db = dbproxy.db.connection;
 	db.on('error', console.error.bind(console, 'Connection error:'));
 	db.once('open', function() {
 		console.log("Connected !");
